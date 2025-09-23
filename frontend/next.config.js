64d677983f94363ff92e890c940d2e9254233d6d
Zainab-Ayoub <zainabzafar365@gmail.com>
2025-09-23 23:46:30 +0500

    final submission

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // Enable static export for Render Static Sites
+  output: 'export',
 };
 
 module.exports = nextConfig;
